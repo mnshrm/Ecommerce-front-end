fe0b9c7cc853b3926563c67287508fee97416b3b
mnshrm <asysnsrs665@gmail.com>
2022-11-16 18:59:57 +0530

    upload on git

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ function Navbar() {
               <button className="avatarLink noButtonStyle">
                 <img
                   className="avatar"
-                  src="https://media-exp2.licdn.com/dms/image/D4D35AQHQMJ-_tcRwCw/profile-framedphoto-shrink_100_100/0/1655535867905?e=1658761200&v=beta&t=UGiWRovNt2Gl-GmuH7CI-H4s4cVeOs-5V4qUuWjBkJc"
+                  src="https://media-exp2.licdn.com/dms/image/https://media-exp1.licdn.com/dms/image/D4D35AQHQMJ-_tcRwCw/profile-framedphoto-shrink_100_100/0/1655535867905?e=1666281600&v=beta&t=q2RJ_S4EzwOBDuhB1kC_r7Pw6XNvHFkDjbBXav-uXxg-_tcRwCw/profile-framedphoto-shrink_100_100/0/1655535867905?e=1658761200&v=beta&t=UGiWRovNt2Gl-GmuH7CI-H4s4cVeOs-5V4qUuWjBkJc"
                   alt="error"
                 />
               </button>
